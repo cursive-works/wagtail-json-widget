def24be1f39e129a3f48ad42f4dc159ecd99d6f6
Patrick Smith <pat@waldosmith.com>
2022-04-05 17:57:13 +0100

    Added install insruction and tidy

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Wagtail JSON Widget
 
-JSON editing for Wagtail's admin.
+JSON editing for Wagtail Admin.
+
+## Installation
+
+Install from [PyPI](https://pypi.org/project/wagtail-json-widget/):
+
+```
+pip install wagtail-json-widget
+```
+
+Then add the following to your project's `INSTALLED_APPS`.
+
+```
+'wagtail_json_widget',
+```
--- wagtail_json_widget/widgets.py
+++ wagtail_json_widget/widgets.py
@@ -20,19 +20,10 @@ class JsonWidgetAdapter(WidgetAdapter):
     class Media:
         js = ['wagtail_json_widget/js/json_widget.js']
 
+
 class JsonEditorWidget(forms.Widget):
-    class Media:
-        js = (
-            getattr(settings, "JSON_EDITOR_JS", 'dist/jsoneditor.min.js'),
-        )
-        css = {
-            'all': (
-                getattr(settings, "JSON_EDITOR_CSS", 'dist/jsoneditor.min.css'),
-            )
-        }
-
     template_name = 'wagtail_json_widget.html'
-
+    
     def __init__(self, attrs=None, mode='code', options=None, width=None, height=None):
         default_options = {
             'modes': ['text', 'code', 'tree', 'form', 'view'],
@@ -50,5 +41,15 @@ class JsonEditorWidget(forms.Widget):
         context = super().get_context(name, value, attrs)
         context['widget']['options'] = json.dumps(self.options)
         return context
+
+    class Media:
+        js = (
+            getattr(settings, "JSON_EDITOR_JS", 'dist/jsoneditor.min.js'),
+        )
+        css = {
+            'all': (
+                getattr(settings, "JSON_EDITOR_CSS", 'dist/jsoneditor.min.css'),
+            )
+        }
 
 register(JsonWidgetAdapter(), JsonEditorWidget)
